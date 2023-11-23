49711564e7886310e0464644c6002cd7a1abe099
Anibal Morillo <anibalmorillo1@gmail.com>
2023-11-23 18:49:51 -0400

    ya diego

---
+++ src/main/java/com/mycompany/javayendry/JavaYendry.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javayendry;
+
+/**
+ *
+ * @author Anibal
+ */
+public class JavaYendry {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
